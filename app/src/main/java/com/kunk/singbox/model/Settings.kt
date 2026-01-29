a83253b6388cd3536634aa991d244184009ccbb4
theyusa <108272816+theyusa@users.noreply.github.com>
2026-01-29 03:48:57 +0300

    Update Settings.kt

--- app/src/main/java/com/kunk/singbox/model/Settings.kt
+++ app/src/main/java/com/kunk/singbox/model/Settings.kt
@@ -216,7 +216,8 @@ enum class AppThemeMode(@StringRes val displayNameRes: Int) {
 enum class AppLanguage(@StringRes val displayNameRes: Int, val localeCode: String) {
     @SerializedName("SYSTEM") SYSTEM(R.string.language_system, ""),
     @SerializedName("CHINESE") CHINESE(R.string.language_chinese, "zh"),
-    @SerializedName("ENGLISH") ENGLISH(R.string.language_english, "en");
+    @SerializedName("ENGLISH") ENGLISH(R.string.language_english, "en"),
+    @SerializedName("TURKISH") TURKISH(R.string.language_turkish, "tr");
 
     companion object {
         fun fromLocaleCode(code: String): AppLanguage {
@@ -228,6 +229,7 @@ enum class AppLanguage(@StringRes val displayNameRes: Int, val localeCode: Strin
         }
     }
 }
+
 
 enum class GhProxyMirror(val url: String, @StringRes val displayNameRes: Int) {
     @SerializedName("SAGERNET_ORIGIN") SAGERNET_ORIGIN("https://raw.githubusercontent.com/", R.string.gh_mirror_sagernet),
